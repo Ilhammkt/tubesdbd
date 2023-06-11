08473c14c40d71eaf612bd2fb6f7eff416445ef6
CettaMa <93418128+CettaMa@users.noreply.github.com>
2023-06-11 14:46:13 +0700

    updateon regis and log in

---
+++ makanan.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kantin Kuy!</title>
+    <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+    <div class="konten">
+        <img class="logo" src="css/logo.png"></li>
+        <div class="navbar">
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+        </div>
+        <a class="shape"></a>
+        <a class="shape1"></a>
+        <a class="shape2"></a>
+        <a class="shape3"></a>
+        <img src="css/gambarawal.svg" class="image7">
+        <div class="title">
+            <h3>Makan &</h3>
+            <h4>Minum dengan</h4>
+            <h5>Cita Rasa Bintang Lima</h5>
+            <a1 href="#">Lihat Menu</a1>
+        </div>
+    </div>
+</body>
+
+</html>
